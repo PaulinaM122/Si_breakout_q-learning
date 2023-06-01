38e04f686158c1a066ef25ac6ce609e7e3ff40dc
PaulinaM122 <paulina.machcinska122@wp.pl>
2023-06-01 11:39:03 +0200

    Ponowne dodanie deep_q_learning do projektu - > dalej nie działa :(

---
+++ agents/deep_qlearning_agent.py
@@ -0,0 +1,131 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from agents.q_learning_agent import QLearningAgent
+from globals.direction import Direction as Dir
+from globals.constants import *
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+
+ACTION_SIZE = 3  # Left, Right and Stay
+STATE_SIZE = 2 #  ball direction and relative position
+
+
+class DeepQLearningAgent(QLearningAgent):
+    def __init__(self):
+        super().__init__(q_values_file_name='q_values_deep.txt', screen_width=SCREEN_WIDTH_SMALL)
+        self.model = self.build_model()
+        self.replay_memory = []  # pamięć powtórek, przechowuje state,q_value
+        self.q_values_history = {}
+    def get_state(self, ball, paddle, bricks):
+        relative_position = [ball.get_relative_position(paddle).value]
+        direction = ball.get_direction().value
+        state_array = np.array(relative_position + [direction])
+        return state_array
+
+    def build_model(self):
+        model = Sequential()
+        model.add(Dense(32, input_shape=(STATE_SIZE,), activation='relu'))  # pierwsza warstwa modelu
+        model.add(Dense(32, activation='relu'))  # druga warstwa modelu
+        model.add(Dense(ACTION_SIZE, activation='linear'))  # trzecia warstwa modelu
+        model.compile(loss='mse', optimizer=Adam(lr=self.alpha))  # Kompiluj model z odpowiednią funkcją straty i optymalizatorem
+        return model
+
+    def get_q_value(self, state, action):
+        state_array = np.array(state)  # wejscia sieci neuronowej
+        q_values = self.model.predict(state_array.reshape(1, -1))  # predict oblicza wartość q-funkcji, wykonując propagację w przód przez sieć neuronową
+        # propagacja w przód to proces przekazywania danych przez sieć od warstwy wejściowej do warstwy wyjściowej, gdzie każda warstwa wykonuje obliczenia na podstawie wag i funkcji aktywacji
+        if state[0] == Dir.STAY.value:
+            this_state = "STAY"
+        elif state[0] == Dir.LEFT.value:
+            this_state = "LEFT"
+        elif state[0] == Dir.RIGHT.value:
+            this_state = "RIGHT"
+        if state[1] == Dir.UP_LEFT.value:
+            this_state2 = "UP_LEFT"
+        elif state[1] == Dir.UP_RIGHT.value:
+            this_state2 = "UP_RIGHT"
+        elif state[1] == Dir.DOWN_LEFT.value:
+            this_state2 = "DOWN_LEFT"
+        elif state[1] == Dir.DOWN_RIGHT.value:
+            this_state2 = "DOWN_RIGHT"
+        if action + 1 == Dir.STAY.value:
+            this_action = "STAY"
+        elif action + 1 == Dir.LEFT.value:
+            this_action = "LEFT"
+        elif action + 1 == Dir.RIGHT.value:
+            this_action = "RIGHT"
+        print(
+            f" position: {this_state}, direction: {this_state2} for action {this_action} - q_value: {q_values[0][action]}")
+        return q_values[0][action]  # wartość dla konkretnej akcji
+
+    def update_q_value(self, state, action, next_state, reward, paddle):
+        state_array = np.array(state)
+        next_state_array = np.array(next_state)
+        q_values = np.copy(self.model.predict(state_array.reshape(1, -1))[0])  # propagacja w przód
+        next_q_values = self.model.predict(next_state_array.reshape(1, -1))[0]  # propagacja w przód
+        q_values[action.value - 1] = reward + self.gamma * np.max(next_q_values)
+
+        # dodaj do pamięci powtórek
+        self.replay_memory.append((state_array.copy(), q_values.copy()))
+
+        # Inicjalizacja q_values_history dla kombinacji (position, direction, action), jeśli jeszcze nie istnieje
+        if not hasattr(self, 'q_values_history'):
+            self.q_values_history = {}
+
+        # Aktualizacja q_values_history dla kombinacji (position, direction, action)
+        state_key = (state[0], state[1], action.value)
+        if state_key not in self.q_values_history:
+            self.q_values_history[state_key] = []
+        self.q_values_history[state_key].append(q_values[action.value - 1])
+
+        # trening sieci na losowej próbce z pamięci powtórek
+        batch_size = 32
+        if len(self.replay_memory) >= batch_size:
+            batch = random.sample(self.replay_memory, batch_size)
+            states = np.array([sample[0] for sample in batch])
+            target_q_values = np.array([sample[1] for sample in batch])
+            self.model.fit(states, target_q_values, epochs=10, verbose=0) # fit dokonuje aktualizacji wag sieci, aby minimalizować różnicę między przewidywanymi a oczekiwanymi wartościami Q-funkcji
+
+    def get_best_action(self, state, paddle):
+        possible_actions = self.get_possible_actions(paddle)
+        possible_q_values = [self.get_q_value(state, action.value - 1) for action in possible_actions]
+
+        max_q_value = np.max(possible_q_values)
+        best_action_indices = np.where(np.isclose(possible_q_values, max_q_value))[0]
+
+        if len(best_action_indices) > 1:
+            return Dir.STAY
+        else:
+            best_action_index = random.choice(best_action_indices)
+
+        return possible_actions[best_action_index]
+
+    def generate_q_value_plots(self):
+        positions = [Dir.LEFT, Dir.RIGHT, Dir.STAY]
+        directions = [Dir.UP_LEFT, Dir.UP_RIGHT, Dir.DOWN_LEFT, Dir.DOWN_RIGHT]
+        actions = [Dir.LEFT, Dir.RIGHT, Dir.STAY]
+
+        for direction in directions:
+            fig, ax = plt.subplots()
+            ax.set_title(f'Direction: {direction.name}')
+
+            for position in positions:
+                for action in actions:
+                    state_key = (position.value, direction.value, action.value)
+                    if state_key in self.q_values_history:
+                        q_values_history = self.q_values_history[state_key]
+                        episodes = np.arange(1, len(q_values_history) + 1)
+
+                        label = f'Position: {position.name}, Action: {action.name}'
+                        ax.plot(episodes, q_values_history, label=label)
+
+            ax.set_xlabel('Episode')
+            ax.set_ylabel('Q-value')
+            ax.legend()
+
+            plt.show()
---
+++ deep_main.py
@@ -0,0 +1,238 @@
+import turtle as tr
+from items.paddle import Paddle
+from items.ball import Ball
+from view.scoreboard import Scoreboard
+from view.ui import UI
+from items.bricks import Bricks
+from agents.deep_qlearning_agent import DeepQLearningAgent
+from globals.direction import Direction as Dir
+from globals.constants import *
+import time
+
+width = SCREEN_WIDTH_SMALL
+height = SCREEN_HEIGHT
+
+screen = tr.Screen()
+screen.setup(width=width, height=height)
+screen.bgcolor('black')
+screen.title('Breakout')
+screen.tracer(0)
+
+ui = UI()
+ui.header()
+
+score = Scoreboard(lives=3)
+paddle = Paddle()
+bricks = Bricks(screen_width=width)
+
+ball = Ball()
+
+playing_game = True
+training_agent = True
+
+# create Deep Q-learning agent
+agent = DeepQLearningAgent()
+#agent.load_q_values()
+
+
+def check_collision_with_walls():
+    global width, height, ball
+
+    # detect collision with left and right walls:
+    edge = width/2 - BALL_DIAMETER
+
+    if ball.xcor() < -edge and ball.x_move_dist < 0 or ball.xcor() > edge and ball.x_move_dist > 0:
+        ball.bounce(x_bounce=True, y_bounce=False)
+        return
+
+    # detect collision with upper wall
+    ceiling = height/2 - BALL_DIAMETER
+    if ball.ycor() > ceiling and ball.y_move_dist > 0:
+        ball.bounce_upper_wall(x_bounce=False, y_bounce=True)
+        return
+
+
+def check_collision_with_bottom_wall():
+    global ball, score, playing_game, ui, height
+
+    # In this case, user failed to hit the ball
+    # thus he loses. The game resets.
+    if ball.ycor() < -(height/2 - BALL_DIAMETER) and ball.y_move_dist < 0:
+        ball.reset()
+        score.decrease_lives()
+        if score.lives == 0:
+            score.increase_game()
+            score.reset()
+            playing_game = False
+            ui.game_over(win=False)
+        ui.change_color()
+        return True
+    return False
+
+
+def check_collision_with_paddle():
+    global ball, paddle
+    # record x-axis coordinates of ball and paddle
+    paddle_x = paddle.xcor()
+    ball_x = ball.xcor()
+
+    # check if ball's distance(from its middle)
+    # from paddle(from its middle) is less than
+    # width of paddle and ball is below a certain
+    # coordinate to detect their collision
+    if ball.distance(paddle) <= HALF_PADDLE \
+            and ball.ycor() <= PADDLE_Y_POS + PADDLE_HEIGHT \
+            and ball.y_move_dist < 0:
+
+        # If Paddle is on Right of Screen
+        if paddle_x > 0:
+            if ball_x > paddle_x:
+                # If ball hits paddles left side it
+                # should go back to left
+                ball.bounce(x_bounce=True, y_bounce=True)
+                return True
+            else:
+                ball.bounce(x_bounce=False, y_bounce=True)
+                return True
+
+        # If Paddle is left of Screen
+        elif paddle_x < 0:
+            if ball_x < paddle_x:
+                # If ball hits paddles left side it
+                # should go back to left
+                ball.bounce(x_bounce=True, y_bounce=True)
+                return True
+            else:
+                ball.bounce(x_bounce=False, y_bounce=True)
+                return True
+
+        # Else Paddle is in the Middle horizontally
+        else:
+            if ball_x > paddle_x:
+                ball.bounce(x_bounce=True, y_bounce=True)
+                return True
+            elif ball_x < paddle_x:
+                ball.bounce(x_bounce=True, y_bounce=True)
+                return True
+            else:
+                ball.bounce(x_bounce=False, y_bounce=True)
+                return True
+    return False
+
+
+def check_collision_with_bricks():
+    global ball, score, bricks
+
+    for brick in bricks.bricks:
+        if ball.distance(brick) < 40:
+            score.increase_score()
+            brick.clear()
+            brick.goto(3000, 3000)
+            bricks.bricks.remove(brick)
+
+            # detect collision from left
+            if ball.xcor() < brick.left_wall:
+                ball.bounce(x_bounce=True, y_bounce=False)
+
+            # detect collision from right
+            elif ball.xcor() > brick.right_wall:
+                ball.bounce(x_bounce=True, y_bounce=False)
+
+            # detect collision from bottom
+            elif ball.ycor() < brick.bottom_wall:
+                ball.bounce(x_bounce=False, y_bounce=True)
+
+            # detect collision from top
+            elif ball.ycor() > brick.upper_wall:
+                ball.bounce(x_bounce=False, y_bounce=True)
+
+            return
+
+
+def reset_the_game():
+    global score, bricks, ball, paddle, playing_game
+
+    score.lives = 3
+    bricks.reset()
+    ball.reset()
+    paddle.goto(x=0, y=PADDLE_Y_POS)
+    playing_game = True
+
+
+def leave_game():
+    global playing_game, training_agent
+    playing_game = False
+    training_agent = False
+
+
+screen.listen()
+screen.onkey(key='Left', fun=paddle.move_left)
+screen.onkey(key='Right', fun=paddle.move_right)
+screen.onkey(key='Escape', fun=leave_game)
+
+
+while training_agent:
+    reset_the_game()
+
+    # start a new game
+    while playing_game:
+        reward = 0
+        ball.move()
+        state = agent.get_state(ball, paddle, bricks)
+        action = agent.get_action(state, paddle)
+
+        # perform action
+        if action == Dir.LEFT:
+            paddle.move_left()
+        elif action == Dir.RIGHT:
+            paddle.move_right()
+        else:
+            reward += 1
+
+        # UPDATE SCREEN WITH ALL THE MOTION THAT HAS HAPPENED
+        screen.update()
+        #time.sleep(0.01)
+
+        # DETECTING COLLISION WITH WALLS
+        check_collision_with_walls()
+        if check_collision_with_bottom_wall():
+            reward += -1 * abs(ball.xcor() - paddle.xcor())
+            agent.success_history.append(0)
+
+        # DETECTING COLLISION WITH THE PADDLE
+        if check_collision_with_paddle():
+            reward += 100
+
+        # DETECTING COLLISION WITH A BRICK
+        check_collision_with_bricks()
+
+        # DETECTING USER'S VICTORY
+        if len(bricks.bricks) == 0:
+            ui.game_over(win=True)
+            playing_game = False
+            agent.success_history.append(1)
+
+        agent.reward_history[agent.num_games] += reward
+
+        # update Q-values
+        next_state = agent.get_state(ball, paddle, bricks)
+        agent.update_q_value(state, action, next_state, reward, paddle)
+
+    # check if the agent won the game
+    if len(bricks.bricks) == 0:
+        ui.game_over(win=True)
+    else:
+        # agent lost the game, start over
+        ui.game_over(win=False)
+
+    ui.saving_q_values()
+    screen.update()
+    agent.save_q_values()
+
+    agent.increase_num_games()
+
+    agent.generate_q_value_plots()
+
+    # check if the maximum number of games has been reached
+    if agent.num_games >= agent.max_num_games:
+        break
